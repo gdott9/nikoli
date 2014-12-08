8fd83c8aa768065c825cb5e08d11adb395511a2f
Guillaume Dott <guillaume+github@dott.fr>
2014-12-08 18:02:28 +0100

    Use XMLHttpRequest to load Nurikabe game data

--- nikoli.rb
+++ nikoli.rb
@@ -2,12 +2,16 @@
 
 require 'sinatra'
 require 'sinatra/content_for'
+require 'sinatra/json'
 
+require 'yaml'
 require 'coffee-script'
 require 'slim'
 require 'sass'
 
 GAMES = %i{hitori nurikabe sudoku}
+
+set :data_folder, File.expand_path('./data')
 
 get('/application.css') { scss :application }
 get('/application.js') { coffee :application }
@@ -17,4 +21,11 @@ get('/') { slim :index, locals: {games: GAMES} }
 GAMES.each do |game|
   get("/#{game}") { slim game }
   get("/#{game}.js") { coffee game }
+end
+
+get "/data/:game/:file.json" do |game, file|
+  data_file = File.join(settings.data_folder, game, "#{file}.yml")
+  halt(404) unless File.exist?(data_file)
+
+  json YAML.load_file(data_file).sample
 end
